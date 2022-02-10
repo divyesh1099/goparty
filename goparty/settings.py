07b3bbef691c0d2c383a2fe2dfa946d124a84a32
Divyesh Vishwakarma <divyesh1099@gmail.com>
2022-02-11 02:55:05 +0530

    Allowed hosts settings changed again

--- goparty/settings.py
+++ goparty/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "goparty.co.in"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "goparty.co.in", "goparty-mz7kc.ondigitalocean.app"]
 
 
 # Application definition
