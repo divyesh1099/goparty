cea6003fe0c71046962d9579706c27364c823988
Divyesh Vishwakarma <divyesh1099@gmail.com>
2022-04-15 16:15:35 +0530

    Data fields created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.db import models
 from django_google_maps import fields
 # Create your models here.
@@ -6,6 +7,11 @@ class Restaurant(models.Model):
     name = models.CharField(max_length=100, unique=True)
     address = fields.AddressField(max_length=200)
     geolocation = fields.GeoLocationField(max_length=100)
+    price = models.IntegerField(default=1000, blank=True, null=True)
+    start_time = models.TimeField(blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+    edited = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    created = models.DateTimeField(auto_now=True, blank=True, null=True)
     def __str__(self):
         return self.name
 
