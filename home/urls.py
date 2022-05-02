a4962893056dae2e27a177a1a085457c4794885e
Divyesh Vishwakarma <divyesh1099@gmail.com>
2022-05-02 23:34:57 +0530

    Footer and Searchbar Completed

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('login', views.login, name = 'login'),
+    path('signup', views.signup, name = 'signup'),
     ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def index(request):
     restaurant_list = Restaurant.objects.all()
     context = {'restaurant_list':restaurant_list}
     return render(request, 'home/index.html', context)
+
+def login(request):
+    return render(request, 'home/login.html')
+
+def signup(request):
+    return render(request, 'home/signup.html')
